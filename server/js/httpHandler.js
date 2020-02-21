4dd0fbcbc223bc49b5456dbe12dc1a8fe04cfc72
Vincent Huang <vincent.kim.huang@gmail.com>
2020-02-21 15:18:07 -0800

    Add functionality for random command on refresh and write corresponding test

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -15,6 +15,19 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
-  res.end();
+
+  switch (req.method) {
+    case 'GET':
+      //random command
+      var commands = ['up', 'down', 'left', 'right'];
+      var index = Math.floor(Math.random() * commands.length);
+
+      res.end(commands[index]);
+
+    default:
+      res.end();
+  }
+
   next(); // invoke next() at the end of a request to help with testing!
+
 };
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -22,13 +22,22 @@ describe('server responses', () => {
   });
 
   it('should respond to a GET request for a swim command', (done) => {
-    // write your test here
+
+    let commands = ['up', 'down', 'left', 'right'];
+
+    let {req, res} = server.mock('/', 'GET');
+
+    httpHandler.router(req, res);
+    expect(res._responseCode).to.equal(200);
+    expect(res._ended).to.equal(true);
+    expect(commands).to.contain(res._data.toString());
+
     done();
   });
 
-  xit('should respond with 404 to a GET request for a missing background image', (done) => {
+  it('should respond with 404 to a GET request for a missing background image', (done) => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
-    let {req, res} = server.mock('FILL_ME_IN', 'GET');
+    let {req, res} = server.mock(httpHandler.backgroundImageFile, 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(404);
